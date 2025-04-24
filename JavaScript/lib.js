9046466ab79f668d78963d19f86588acbb89743e
Nesslia <vanessajentzsch@googlemail.com>
2025-04-24 13:15:59 +0200

    js aufgabe 1-3

--- JavaScript/lib.js
+++ JavaScript/lib.js
@@ -3,13 +3,24 @@ let name = "Max";
 let nachname = 'Mustermann';
 let vollständigerName = name + " " + nachname;
 console.log(vollständigerName);
+function zeigeVollständigerName() {
+    alert(vollständigerName);
+}
 
 // Station 2: Zahlen
 let ganzzahl = 42;
 let kommazahl = 3.14;
 let ergebnis = ganzzahl + kommazahl;
 console.log(ergebnis);
+function zeigeErgebnisse() {
+    alert(ergebnis);
+}
 
+
+function wahl() {
+    console.log("UND Verknüpfung" + (istWahr && istFalsch) + " links müsste falsch rauskommen")
+    console.log("ODER Verknüpfung" + (istWahr || istFalsch) + " links müsste richtig rauskommen")
+}
 
 // Station 3: Booleans
 let istWahr = true;
@@ -18,6 +29,7 @@ console.log(istWahr && istFalsch); // UND
 console.log(istWahr || istFalsch); // ODER
 
 // Station 4: Arrays
+
 let farben = ["rot", "grün", "blau"];
 console.log(farben[0]); // Erstes Element
 farben.push("gelb"); // Neues Element hinzufügen
