6003e63df72294c8564a56c3a9995ade54651b63
ReikyX <188848574+ReikyX@users.noreply.github.com>
2025-04-24 13:04:45 +0200

    Übung JavaScript erledigt

--- JavaScript/lib.js
+++ JavaScript/lib.js
@@ -1,23 +1,34 @@
 // Station 1: Strings
+function vollstaendigername(){
 let name = "Max";
-let nachname = 'Mustermann';
+let nachname = "Mustermann";
 let vollständigerName = name + " " + nachname;
 console.log(vollständigerName);
+}
+
 
 // Station 2: Zahlen
+function ergebnis(){
 let ganzzahl = 42;
 let kommazahl = 3.14;
 let ergebnis = ganzzahl + kommazahl;
 console.log(ergebnis);
-
+}
 
 // Station 3: Booleans
+function wahroderfalsch(){
 let istWahr = true;
 let istFalsch = false;
 console.log(istWahr && istFalsch); // UND
 console.log(istWahr || istFalsch); // ODER
+}
 
 // Station 4: Arrays
-let farben = ["rot", "grün", "blau"];
+function allefarben(){
+let ifarben = document.getElementById("inputForColours").value;
+
 console.log(farben[0]); // Erstes Element
-farben.push("gelb"); // Neues Element hinzufügen
+farben.push(ifarben); // Neues Element hinzufügen 
+console.log(farben);
+}
+let farben = ["rot", "grün", "blau"];
