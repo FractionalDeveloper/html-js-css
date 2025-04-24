e036c54a3078daa0a6e2e627409d1a4e3d2bb907
Melanie Schadl <electronicmeli@gmail.com>
2025-04-24 10:02:10 +0200

    js aufgabe

--- TranceMeli/JavaScript/learning.js
+++ TranceMeli/JavaScript/learning.js
@@ -1,1 +1,16 @@
-alert("Hello I am a alert box!");
+
+
+function zeigeNachricht() 
+{
+    alert("Hello I am a alert box!");
+}
+
+function loggeInKonsole() 
+{
+    console.log("Test blabla")
+}
+
+function textAendern() 
+{
+    document.getElementById("veraenderbarer-text").textContent="Text wurde geändert!"
+}
