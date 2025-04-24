cf7b0c69528c797adb331f0aef412e480a09c966
Melanie Schadl <electronicmeli@gmail.com>
2025-04-24 16:46:05 +0200

    more work

--- JavaScript/lib.js
+++ JavaScript/lib.js
@@ -2,7 +2,7 @@
 let name = "Max";
 let nachname = 'Mustermann';
 let vollständigerName = name + " " + nachname;
-console.log(zeigeVollständigerName);
+
 function zeigeVollständigerName(){
     alert(vollständigerName)
 }
@@ -11,7 +11,7 @@ function zeigeVollständigerName(){
 let ganzzahl = 42;
 let kommazahl = 3.14;
 let ergebnis = ganzzahl + kommazahl;
-console.log(ergebnis);
+
 function zeigeErgebnis(){
     console.log(ergebnis)
 }
@@ -22,6 +22,7 @@ let istWahr = true;
 let istFalsch = false;
 console.log(istWahr && istFalsch); // UND
 console.log(istWahr || istFalsch); // ODER
+
 function zeigeWahrOderFalsch()
 {
 console.log("UND Verknuepfung " + (istWahr && istFalsch) + " links muesste false rauskommen")
@@ -29,12 +30,13 @@ console.log("ODER Verknuepfung " + (istFalsch || istFalsch) + " links muesste tr
 }
 
 // Station 4: Arrays
-function alleFarben(){
-    let ifarben = document.getElementById("inputForColours")
-
-
 console.log(farben[0]); // Erstes Element
 farben.push("gelb"); // Neues Element hinzufügen
-console.log(farben);
-}
 let farben = ["rot", "grün", "blau"];
+
+function alleFarben()
+{ 
+    let inputFeld = document.getElementById("inputForColours")
+    farben.push(inputFeld.value)
+    console.log(farben)   
+}
--- basics/app.js
+++ basics/app.js
@@ -6,12 +6,50 @@ const checkButton = document.getElementById('checkButton');
 
 // Event-Listener für den Button
 checkButton.addEventListener('click', function() {
+    console.log(ageInput.value);
+    const age = ageInput.value;
+    const isAllowed = checkEntryTime(age)
+    showResult(age, isAllowed);
+    ageInput.value = "";
 });
 
 // Funktion zur Überprüfung der Eintrittszeit
 function checkEntryTime(age) {
+const currentHour = newDate().getHours();
+if(age >= 1 && age < 12)
+{
+    if(currentHour >= 8 || currentHour <= 12)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+else if (age >=12 && age <= 17) 
+{
+    if(currentHour >= 12 || currentHour <= 16)
+    {
+        return true;
+    }
+    else
+    {
+
+        return false;
+    }
+} 
+else 
+{      
+    return false;
+}
 }
 
 // Funktion zum Anzeigen des Ergebnisses
-function showResult(age, isAllowed) {
+function showResult(age, isAllowed) 
+{
+    let resultHtml = `<h2>${age}</h2>`
 }
+
+resultSection.innerHTML= resultHtml;
+resultSection.classList.remove("hidden");
